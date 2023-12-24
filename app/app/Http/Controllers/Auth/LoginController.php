988f50147cbc19674a4c676b912bb5d6e50ec88e
Muhammad Erwin Arif <muherwinarif31@gmail.com>
2023-12-24 19:10:40 +0800

    feat: authentication

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -4,10 +4,32 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            return redirect()->route('user.home');
+        }
+
+        return redirect()->back()->with('error', 'Email atau password salah');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
     }
 }

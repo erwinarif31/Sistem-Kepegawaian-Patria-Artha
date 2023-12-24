be897fbc6c38daa762f7096ead4ead4ac71d92c1
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-12-25 01:06:51 +0800

    bidang-keilmuan

--- app/app/Http/Controllers/User/Profile/ProfileController.php
+++ app/app/Http/Controllers/User/Profile/ProfileController.php
@@ -10,4 +10,8 @@ class ProfileController extends Controller
     public function index(){
         return view('user.profile.data-pribadi');
     }
+
+    public function bidangKeilmuan(){
+        return view('user.profile.data-pribadi.bidang-keilmuan');
+    }
 }

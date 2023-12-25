60e3047fb51ffddf3eb31ba1337413ed6a93873a
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-12-25 21:23:35 +0800

    js for alamat-kontak

--- app/resources/views/user/profile/data-pribadi/alamat-kontak.blade.php
+++ app/resources/views/user/profile/data-pribadi/alamat-kontak.blade.php
@@ -114,6 +114,62 @@
                 </td>
             </tr>
         </table>
+
+        <div class="row">
+            <div class="col-lg-4" style="font-size: 12px">
+                <p class="mb-0">Upload Dokumen</p>
+                <p class="mb-0" style="font-style: italic">(Maksimal total ukuran file dalam sekali proses upload: 5 MB)</p>
+                <br>
+                <p class="mb-0" style="font-style: italic; color: cornflowerblue">Dokumen yang dilampirkan adalah dokumen wajib dan dokumen yang sesuai dengan data yang diusulkan.</p>
+                <br>
+                <p class="mb-0" style="font-style: italic; color: cornflowerblue">Dokumen Wajib:</p>
+                <p class="mb-0" style="font-style: italic; color: cornflowerblue">- KTP</p>
+            </div>
+
+            <div class="col border pb-3 pl-0 pr-0" style="font-size: 14px">
+                <p class="bg-secondary p-1 pl-2 font-weight-bold">Dokumen 1</p>
+                <div class="pl-2 pr-2">
+                    <p class="m-0 font-weight-bold">
+                        File
+                        <svg style="fill: red; padding-bottom: 2px" xmlns="http://www.w3.org/2000/svg" height="8" width="8" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M208 32c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V172.9l122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.5 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1V480c0 17.7-14.3 32-32 32H240c-17.7 0-32-14.3-32-32V339.1L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4V32z"/></svg>
+                    </p>
+                    <p class="font-italic m-0 pb-1" style="font-size: 12px">(Jenis file yang diijinkan: pdf, jpg, jpeg, png, doc, docx, xls, xlsx, txt)</p>
+                    <input type="file" class="w-100" placeholder="Pilih file">
+
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Nama Dokumen</p>
+                    <input type="text" class="w-100">
+
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Keterangan</p>
+                    <input type="text" class="w-100">
+                    
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Jenis Dokumen</p>
+                    <select class="form-select w-100 mt-1" aria-label="Small select example">
+                        <option selected>Pilih...</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Tautan Dokumen</p>
+                    <input type="text" class="w-100">
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-2 mb-2" id="parent">
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4" style="font-size: 12px">
+            </div>
+            <div class="col d-flex justify-content-between" style="font-size: 14px">
+                <button id="tambah" class="btn p-0 font-weight-bold" style="color: cornflowerblue; font-size: 14px">Tambah dokumen baru...</button>
+                <button class="btn btn-primary" style="fill: white; font-size: 12px">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"/></svg>
+                    Simpan
+                </button>
+            </div>
+        </div>
     </div>
 @stop
 
@@ -123,4 +179,50 @@
 
 @section('js')
     <script> console.log('Hi!'); </script>
+    <script>
+        let num = 1;
+
+        $('#tambah').click(() =>{
+            num++;
+            inputData(num);
+        })
+        
+        inputData = (i) => {
+            let tr = `<div class="row">
+            <div class="col-lg-4" style="font-size: 12px">
+            </div>
+
+            <div class="col mt-2 mb-2 border pb-3 pl-0 pr-0" style="font-size: 14px">
+                <p class="bg-secondary p-1 pl-2 font-weight-bold">Dokumen <span id="${i}">${num}</span></p>
+                <div class="pl-2 pr-2">
+                    <p class="m-0 font-weight-bold">
+                        File
+                        <svg style="fill: red; padding-bottom: 2px" xmlns="http://www.w3.org/2000/svg" height="8" width="8" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M208 32c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V172.9l122-70.4c15.3-8.8 34.9-3.6 43.7 11.7l16 27.7c8.8 15.3 3.6 34.9-11.7 43.7L352 256l122 70.4c15.3 8.8 20.5 28.4 11.7 43.7l-16 27.7c-8.8 15.3-28.4 20.6-43.7 11.7L304 339.1V480c0 17.7-14.3 32-32 32H240c-17.7 0-32-14.3-32-32V339.1L86 409.6c-15.3 8.8-34.9 3.6-43.7-11.7l-16-27.7c-8.8-15.3-3.6-34.9 11.7-43.7L160 256 38 185.6c-15.3-8.8-20.5-28.4-11.7-43.7l16-27.7C51.1 98.8 70.7 93.6 86 102.4l122 70.4V32z"/></svg>
+                    </p>
+                    <p class="font-italic m-0 pb-1" style="font-size: 12px">(Jenis file yang diijinkan: pdf, jpg, jpeg, png, doc, docx, xls, xlsx, txt)</p>
+                    <input type="file" class="w-100" placeholder="Pilih file">
+
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Nama Dokumen</p>
+                    <input type="text" class="w-100">
+
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Keterangan</p>
+                    <input type="text" class="w-100">
+                    
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Jenis Dokumen</p>
+                    <select class="form-select w-100 mt-1" aria-label="Small select example">
+                        <option selected>Pilih...</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+
+                    <p class="m-0 mt-2 font-weight-bold pb-1">Tautan Dokumen</p>
+                    <input type="text" class="w-100">
+                </div>
+            </div>
+        </div>`;
+
+        $('#parent').append(tr);
+        }
+    </script>
 @stop

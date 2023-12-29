5e002faa32f46eae44e85db6e76c122f55f95155
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-12-29 21:36:04 +0800

    fix pendidikan formal

--- app/resources/views/user/kualifikasi/pendidikan-formal/detail.blade.php
+++ app/resources/views/user/kualifikasi/pendidikan-formal/detail.blade.php
@@ -106,6 +106,35 @@
                 </tr>
             </table>
         </div>
+
+        <p class="bg-secondary p-1 pl-2 font-weight-bold m-0" style="font-size: 14px">Dokumen</p>
+        <div class="table-responsive">
+            <table class="table table-bordered" style="font-size: 14px">
+                <tr class="text-center">
+                    <th>Nomor</th>
+                    <th>Nama File</th>
+                    <th>Nama Dokumen</th>
+                    <th>Keterangan</th>
+                    <th>Jenis Dokumen</th>
+                    <th>Tautan Dokumen</th>
+                    <th>Aksi</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SK III-C 2023.pdf</td>
+                    <td>SK III-C 2023</td>
+                    <td>-</td>
+                    <td>SK Pangkat/Inpassing</td>
+                    <td>-</td>
+                    <td>
+                        <a href="" class="" style="fill: cornflowerblue; color: cornflowerblue; font-size: 14px">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                            Lihat Dokumen
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 @stop
 

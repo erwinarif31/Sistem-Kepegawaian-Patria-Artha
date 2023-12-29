285c3a34deadbd52bb8435846df59d5c2cfa6ee3
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-12-29 21:38:22 +0800

    fix edit sertifikasi-dosen

--- app/resources/views/user/kompetensi/sertifikasi/edit-dosen.blade.php
+++ app/resources/views/user/kompetensi/sertifikasi/edit-dosen.blade.php
@@ -94,17 +94,23 @@
             <tr class="text-center">
                 <th>Nomor</th>
                 <th>Nama File</th>
+                <th>Nama Dokumen</th>
+                <th>Keterangan</th>
                 <th>Jenis Dokumen</th>
+                <th>Tautan Dokumen</th>
                 <th>Aksi</th>
             </tr>
             <tr>
                 <td>1</td>
                 <td>SK III-C 2023.pdf</td>
+                <td>SK III-C 2023</td>
+                <td>-</td>
                 <td>SK Pangkat/Inpassing</td>
+                <td>-</td>
                 <td>
-                    <a href="" class="" style="fill: cornflowerblue; color: cornflowerblue; font-size: 14px">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-                        Lihat Dokumen
+                    <a href="" class="" style="fill: crimson; color: crimson; font-size: 14px">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+                        Hapus Dokumen
                     </a>
                 </td>
             </tr>
